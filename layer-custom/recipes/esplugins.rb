b7d91c60a6607cd474d8c0c5f3d09f6f159e208f
Andrew Turner <aturner@esri.com>
2014-07-05 21:58:01 -0400

    Added plugin/elasticsearch-HQ

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -8,4 +8,14 @@ script "install_plugin_es_head" do
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
 
+script "install_plugin_es_head" do
+	interpreter "bash"
+	user "root"
+	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+	code <<-EOH
+  	plugin -install royrusso/elasticsearch-HQ
+  	EOH
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/HQ") }
+end
+
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
